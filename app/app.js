f8b34386c572591a698a50391250b5865606d74d
levyKim <levy.k@buyme.co.il>
2021-10-05 12:07:21 +0300

    add index file rendering

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+app.use(express.static("public"));
 
 const port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
-    res.send('Hello World!11')
+    // res.send('Hello World!11')
+    res.sendfile('index.html');
 })
 app.listen(port, function () {
-    console.log('Example app listening on port: 1-' + port);
+    console.log('Example app listening on: http://localhost:' + port);
 })
